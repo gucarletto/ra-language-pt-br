882a56de639279c96fb8fa97c6844e1677624f78
Paulo Viana <paulo9mv@yahoo.com.br>
2023-02-02 22:21:37 -0400

    refactor: update to ra-core v4.7.2
    add package lock
    
    
    update package json
    
    Create release-package.yml
    Create .npmrc
    Update package.json
    Update package.json
    add package-lock
    
    
    add test
    
    Update release-package.yml
    Update release-package.yml
    Delete .npmrc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { TranslationMessages } from 'ra-core';
+import { TranslationMessages } from 'ra-core'
 
 const ptBrMessages: TranslationMessages = {
   ra: {
@@ -6,30 +6,40 @@ const ptBrMessages: TranslationMessages = {
       add_filter: 'Adicionar filtro',
       add: 'Adicionar',
       back: 'Retornar',
-      bulk_actions:
-        '%{smart_count} selecionado |||| %{smart_count} selecionados',
+      bulk_actions: '%{smart_count} selecionado |||| %{smart_count} selecionados',
       cancel: 'Cancelar',
+      clear_array_input: 'Limpar lista',
       clear_input_value: 'Limpar valor',
       clone: 'Duplicar',
       confirm: 'Confirmar',
       create: 'Criar',
+      create_item: 'Criar %{item}',
       delete: 'Remover',
       edit: 'Alterar',
       export: 'Exportar',
       list: 'Listar',
       refresh: 'Atualizar',
       remove_filter: 'Remover filtro',
+      remove_all_filters: 'Remover todos os filtros',
       remove: 'Remover',
       save: 'Salvar',
       search: 'Buscar',
+      select_all: 'Selecionar todos',
+      select_row: 'Selecionar esta linha',
       show: 'Mostrar',
       sort: 'Ordenar',
       undo: 'Desfazer',
+      unselect: 'Desmarcar',
       expand: 'Expandir',
       close: 'Fechar',
       open_menu: 'Abrir menu',
       close_menu: 'Fechar menu',
-      unselect: 'Desmarcar',
+      update: 'Atualizar',
+      move_up: 'Mover para cima',
+      move_down: 'Mover para baixo',
+      open: 'Abrir',
+      toggle_theme: 'Alternar tema',
+      select_columns: 'Colunas',
     },
     boolean: {
       true: 'Sim',
@@ -39,35 +49,28 @@ const ptBrMessages: TranslationMessages = {
     page: {
       create: 'Criar %{name}',
       dashboard: 'Dashboard',
-      edit: '%{name} #%{id}',
+      edit: '%{name} #%{recordRepresentation}',
       error: 'Ocorreu um problema',
       list: '%{name}',
       loading: 'Carregando',
       not_found: 'Não encontrado',
-      show: '%{name} #%{id}',
+      show: '%{name} #%{recordRepresentation}',
       empty: 'Sem %{name} ainda.',
       invite: 'Você quer adicionar um?',
     },
     input: {
       file: {
-        upload_several:
-          'Solte arquivos para upload, ou clique para selecionar um.',
-        upload_single:
-          'Solte um arquivo para upload, ou clique para selecioná-lo.',
+        upload_several: 'Solte arquivos para upload, ou clique para selecionar um.',
+        upload_single: 'Solte um arquivo para upload, ou clique para selecioná-lo.',
       },
       image: {
-        upload_several:
-          'Solte imagens para upload, ou clique para selecionar uma.',
-        upload_single:
-          'Solte uma imagem para upload, ou clique para selecioná-la.',
+        upload_several: 'Solte imagens para upload, ou clique para selecionar uma.',
+        upload_single: 'Solte uma imagem para upload, ou clique para selecioná-la.',
       },
       references: {
-        all_missing:
-          'Não foi possível encontrar os dados de referência.',
-        many_missing:
-          'Pelo menos uma das referências associadas aparenta não estar mais disponível.',
-        single_missing:
-          'A referência associada aparenta não estar mais disponível.',
+        all_missing: 'Não foi possível encontrar os dados de referência.',
+        many_missing: 'Pelo menos uma das referências associadas aparenta não estar mais disponível.',
+        single_missing: 'A referência associada aparenta não estar mais disponível.',
       },
       password: {
         toggle_visible: 'Ocultar senha',
@@ -77,39 +80,44 @@ const ptBrMessages: TranslationMessages = {
     message: {
       about: 'Sobre',
       are_you_sure: 'Tem certeza?',
+      auth_error: 'Um erro ocorreu durante a validação do token de autenticação.',
       bulk_delete_content:
         'Você tem certeza que quer remover isso %{name}? |||| Você tem certeza que quer remover estes %{smart_count} items?',
-      bulk_delete_title:
-        'Remover %{name} |||| Remover %{smart_count} %{name}',
+      bulk_delete_title: 'Remover %{name} |||| Remover %{smart_count} %{name}',
+      bulk_update_content:
+        'Tem certeza que deseja atualizar este %{name}? |||| Tem certeza que deseja atualizar estes %{smart_count} itens?',
+      bulk_update_title: 'Atualizar %{name} |||| Atualizar %{smart_count} %{name}',
+      clear_array_input: 'Você tem certeza de que deseja limpar toda a lista?',
       delete_content: 'Você tem certeza que quer remover este item?',
       delete_title: 'Remover %{name} #%{id}',
       details: 'Detalhes',
-      error:
-        'Ocorreu um erro no navegador e seu requisição não foi completada.',
-      invalid_form:
-        'O formulário não é válido. Por favor, verifique os erros',
+      error: 'Ocorreu um erro no navegador e seu requisição não foi completada.',
+      invalid_form: 'O formulário não é válido. Por favor, verifique os erros',
       loading: 'A página está carregando, aguarde um momento',
       no: 'Não',
       not_found: 'Você digitou uma URL errada, ou seguiu um link errado.',
       yes: 'Sim',
-      unsaved_changes:
-        'Algumas de suas alteracões não foram salvas. Você tem certeza que quer ignorá-las?',
+      unsaved_changes: 'Algumas de suas alteracões não foram salvas. Você tem certeza que quer ignorá-las?',
     },
     navigation: {
       no_results: 'Nenhum resultado encontrado',
-      no_more_results:
-        'A página número %{page} fora dos limites. Tente a página anterior.',
+      no_more_results: 'A página número %{page} fora dos limites. Tente a página anterior.',
       page_out_of_boundaries: 'A página %{page} está fora dos limites',
       page_out_from_end: 'Fim da paginação',
       page_out_from_begin: 'A página não pode ser menor que 1',
       page_range_info: '%{offsetBegin}-%{offsetEnd} de %{total}',
-      page_rows_per_page: 'Linhas por página:',
+      partial_page_range_info: '%{offsetBegin}-%{offsetEnd} de mais de %{offsetEnd}',
+      current_page: 'Página %{page}',
+      page: 'Ir para página %{page}',
+      first: 'Ir para primeira página',
+      last: 'Ir para ultima página',
       next: 'Próxima',
-      prev: 'Anterior',
+      previous: 'Anterior',
+      page_rows_per_page: 'Linhas por página:',
       skip_nav: 'Ir para o conteúdo',
     },
     sort: {
-      sort_by: 'Ordernar por %{field} %{order}',
+      sort_by: 'Ordenar por %{field} %{order}',
       ASC: 'ascendente',
       DESC: 'descendente',
     },
@@ -123,20 +131,17 @@ const ptBrMessages: TranslationMessages = {
       logout: 'Sair',
     },
     notification: {
-      updated:
-        'Elemento atualizado |||| %{smart_count} elementos atualizados',
+      updated: 'Elemento atualizado |||| %{smart_count} elementos atualizados',
       created: 'Elemento criado',
-      deleted:
-        'Elemento removido |||| %{smart_count} elementos removidos',
+      deleted: 'Elemento removido |||| %{smart_count} elementos removidos',
       bad_item: 'Elemento incorreto',
       item_doesnt_exist: 'O elemento não existe',
       http_error: 'Erro de comunicação com o servidor',
-      data_provider_error:
-        'Erro com o dataProvider. Verifique o console para mais detalhes.',
-      i18n_error:
-        'Não foi possível carregar as traduções para a linguagem especificada',
+      data_provider_error: 'Erro com o dataProvider. Verifique o console para mais detalhes.',
+      i18n_error: 'Não foi possível carregar as traduções para a linguagem especificada',
       canceled: 'Ação cancelada',
       logged_out: 'Sua sessão foi finalizada, por favor reconecte.',
+      not_authorized: 'Você não está autorizado a acessar este recurso.',
     },
     validation: {
       required: 'Obrigatório',
@@ -149,7 +154,43 @@ const ptBrMessages: TranslationMessages = {
       oneOf: 'Deve ser um destes: %{options}',
       regex: 'Deve respeitar um formato específico (regexp): %{pattern}',
     },
+    saved_queries: {
+      label: 'Consultas salvas',
+      query_name: 'Nome da consulta',
+      new_label: 'Salvar consulta atual...',
+      new_dialog_title: 'Salvar consulta atual como...',
+      remove_label: 'Remover consulta salva',
+      remove_label_with_name: 'Remover consulta "%{name}"',
+      remove_dialog_title: 'Remover consulta salva?',
+      remove_message: 'Tem certeza de que deseja remover este item da sua lista de consultas salvas?',
+      help: 'Filtre a lista e salve esta consulta para mais tarde',
+    },
+    configurable: {
+      customize: 'Customizar',
+      configureMode: 'Configurar esta página',
+      Datagrid: {
+        title: 'Datagrid',
+        unlabeled: 'Coluna sem rótulo #%{column}',
+      },
+      inspector: {
+        title: 'Inspetor',
+        content: 'Passe o mouse sobre os elementos da interface do usuário do aplicativo para configurá-los',
+        reset: 'Redefinir configurações',
+        hideAll: 'Ocultar todos',
+        showAll: 'Exibir todos',
+      },
+      SimpleForm: {
+        title: 'Form',
+        unlabeled: 'Unlabeled input #%{input}',
+      },
+      SimpleList: {
+        title: 'Lista',
+        primaryText: 'Texto primário',
+        secondaryText: 'Texto secundário',
+        tertiaryText: 'Texto terciário',
+      },
+    },
   },
-};
+}
 
 export default ptBrMessages
